0a8e4229aa7d91480066f7aa91a170f499ee0948
sepaev <sepaev@gmail.com>
2021-04-08 11:28:21 +0300

    Фикс меню
    Зафиксировал меню + добавил в джаваскрипт закрыватели для ссылок

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,7 +2,11 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const closeMenulink1 = document.querySelector('.link1');
+  const closeMenulink2 = document.querySelector('.link2');
+  const closeMenulink3 = document.querySelector('.link3');
+  const closeMenulink4 = document.querySelector('.link4');
+  
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -17,6 +21,10 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+    closeMenulink1.addEventListener('click', toggleMenu);
+    closeMenulink2.addEventListener('click', toggleMenu);
+    closeMenulink3.addEventListener('click', toggleMenu);
+    closeMenulink4.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
